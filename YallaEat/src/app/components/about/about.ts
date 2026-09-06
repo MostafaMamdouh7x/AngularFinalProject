31d751937af9c72d0f34601fe29f85ce9f307c40
asmaakhalid763 <asmaakhalid783@gmail.com>
2026-09-06 19:03:03 +0300

    update

---
+++ YallaEat/src/app/components/about/about.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-about',
+  styleUrl: './about.css',
+  templateUrl: './about.html',
+})
+export class About {
+}
